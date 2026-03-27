a990feec762d46e1bdd505c5f2882dc7c2b06d61
TPurdy26 <thomascpurdy@icloud.com>
2026-03-27 18:49:16 -0400

    Code completed

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 Math.random
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let odds = Math.floor(Math.random() * 3) + 1;
     let computerChoice;
     switch(odds) {
-        case 1:
+        case 1 :
             computerChoice = "ROCK";
-        case 2:
+            break;
+        case 2 :
             computerChoice = "PAPER";
-        case 3:
+            break;
+        case 3 :
             computerChoice = "SCISSORS";
+            break;
     }
     return computerChoice;
     }
@@ -22,3 +29,38 @@ function getHumanChoice() {
     }
     return humanChoice;
 }
+
+function playRound() {
+    let humanMove = getHumanChoice();
+    let computerMove = getComputerChoice();
+    if (humanMove === "ROCK" && computerMove === "SCISSORS") {
+        alert("Computer's Move: " + computerMove + "\nYou won!");
+        humanScore++;
+    } else if (humanMove === "PAPER" && computerMove === "ROCK") {
+        alert("Computer's Move: " + computerMove + "\nYou won!");
+        humanScore++;
+    } else if (humanMove === "SCISSORS" && computerMove === "PAPER") {
+        alert("Computer's Move: " + computerMove + "\nYou won!");
+        return "USER WON";
+        humanScore++;
+    } else if (humanMove === computerMove) {
+        alert("Computer's Move: " + computerMove + "\n It's a tie!");
+    } else {
+        alert("Computer's Move: " + computerMove + "\nYou lost!");
+        computerScore++;
+    }
+}
+
+function playGame() {
+    while(humanScore + computerScore < 5){
+        playRound();
+        alert("Your score: " + humanScore + "\nComputer score: " + computerScore);
+    }
+    if (humanScore >= computerScore) {
+        alert('You won the game!')
+    } else {
+        alert("You lost :(\nBetter luck next time!")
+    }
+}
+
+playGame()
