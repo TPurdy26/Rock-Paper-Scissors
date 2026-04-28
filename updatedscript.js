41e20c23e99150601437a4fb2428973fcc7dd6a5
TPurdy26 <thomascpurdy@icloud.com>
2026-04-27 21:00:12 -0400

    Files updated

---
+++ updatedscript.js
@@ -0,0 +1,13 @@
+Math.random
+
+const startButton = document.getElementById("play-button");
+const userField = document.querySelector("#user-field");
+const resultsField = document.querySelector("#results-field");
+
+startButton.addEventListener("click", playGame);
+
+function playGame() {
+    startButton.hidden = true;
+    
+    
+}
